027c9e15498f32a8eb1357fe8882b66b5640b25a
Murilo Lombardi <murilomarinato@ucl.br>
2020-08-26 22:27:05 +0000

    Exercício 2

--- main.cs
+++ main.cs
@@ -1,43 +1,59 @@
-/*1- Criar um programa que leia dois números inteiros, e que solicite ao usuário qual a operação 
-deseja realizar entre esses números. Caso o usuário digitar o caractere “*” será realizada uma multiplicação, 
-caso seja digitado o caractere “/” será realizada uma divisão, caso seja digitado o caractere “+” 
-será realizado uma adição, e caso seja digitado o caractere “–” será realizada uma subtração.*/
-
+/*2- O cardápio de uma casa de lanches é dado pela tabela abaixo:
+Código Produto Preço Unitário (R$)
+---------------------------------
+100  Cachorro quente   R$ 1,70
+101   Bauru Simples    R$ 2,30
+102   Bauru com ovo    R$ 2,60
+103   Hamburguer       R$ 2,40
+104   Cheeseburguer    R$ 2,50
+105   Refrigerante     R$ 1,00
+----------------------------------
+Escreva um algoritmo que leia o código do item adquirido pelo consumidor e a
+quantidade, calculando e mostrando o valor a pagar. Não será necessário exibir o produto
+e o valor, somente o valor final*/
+  
 using System;
 
 class MainClass {
-
-public static int Calcula (int n1, int n2, char ope){
-  int resp;
-  
-    switch(ope)
-    {
-      case '+':
-              return n1+n2;
-      case '-':
-              return n1-n2;     
-      case '*':
-              return n1*n2;
-      case '/':
-              return n1/n2;            
-      default:
-              Console.WriteLine("operação invalida!");
-              break;
+  public static double CalculaTotal (int codigo, int q){
+    double preco;
+    if (q<1){
+      Console.WriteLine("Quantidade invalida!!!");
+      return -1;
     }
-  return 0;
-}
+    switch (codigo)
+    {
+     case 100:
+      preco = 1.7;
+      return (preco*Convert.ToSingle(q));
+     case 101:
+      preco = 2.3;
+       return (preco*Convert.ToSingle(q));
+     case 102:
+      preco = 2.6;
+       return (preco*Convert.ToSingle(q));
+     case 103:
+      preco = 2.4;
+      return (preco*Convert.ToSingle(q));
+     case 104:
+      preco = 2.5;
+      return (preco*Convert.ToSingle(q));
+     case 105:
+      preco = 1.0;
+      return (preco*Convert.ToSingle(q));
+     default: 
+      Console.WriteLine("Codigo não cadastrado!!!");
+      return -1; 
+    }
+  } 
+  public static void Main () {
+    
+    int codigo, quantidade;
 
-
-public static void Main (string[] args) 
-{
-  char sinal;
-  int num1=0, num2=0;
-  Console.WriteLine("Digite a operação desejada:  "); 
-  sinal = char.Parse(Console.ReadLine());
-  Console.WriteLine("Digite o numero 1:");
-  num1 = int.Parse(Console.ReadLine());
-  Console.WriteLine("Digite o numero 2:");
-  num2 = int.Parse(Console.ReadLine());
-  Console.WriteLine("O resultado da operação é: {0}", Calcula(num1, num2, sinal));
-}
+    Console.WriteLine("Digite o código do produto: ");
+    codigo = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite a quantidade consumida: ");
+    quantidade = int.Parse(Console.ReadLine());
+    Console.WriteLine("Valor Final: {0} ", CalculaTotal(codigo,quantidade));
+  } 
 }
